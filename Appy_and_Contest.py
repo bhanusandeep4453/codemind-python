86dcd72fa85d79a61b32274bcdb853d28c61b083
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2023-05-20 20:53:47 +0530

    t=int(input()) for _ in range(t):     n,a,b,k=map(int,input().split())     c=0     for i in range(1,n+1):         if i%a==0 and i%b!=0:             c=c+1         elif i%b==0 and i%a!=0:             c=c+1         if c==k:             print("Win")             break     else:         print("Lose")

---
+++ Appy_and_Contest.py
@@ -0,0 +1,14 @@
+t=int(input())
+for _ in range(t):
+    n,a,b,k=map(int,input().split())
+    c=0
+    for i in range(1,n+1):
+        if i%a==0 and i%b!=0:
+            c=c+1
+        elif i%b==0 and i%a!=0:
+            c=c+1
+        if c==k:
+            print("Win")
+            break
+    else:
+        print("Lose")
